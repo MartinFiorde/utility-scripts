02618c6b6eef5eec52f4e74d6b5aa011bdbaa8a2
fiorde90 <fiorde_90@hotmail.com>
2024-11-18 18:21:29 -0300

    add input handler

--- src/single_scripts/new_script.py
+++ src/single_scripts/new_script.py
@@ -1,23 +1,48 @@
 import os
-from tkinter import Tk, filedialog
+import shutil
+from tkinter import Tk, filedialog, messagebox
 
 def start() :
     
     print("hello world")
+    file_path = pick_and_copy_file()
+    if not file_path: return
+    
 
-        # Oculta la ventana principal de Tkinter
-    root = Tk()
-    root.withdraw()
-
-    # Establece el directorio inicial en el Escritorio del usuario
-    desktop_dir = os.path.join(os.path.expanduser("~"), "Desktop")
-
-    # Abre el cuadro de diálogo para seleccionar un archivo
-    file_path = filedialog.askopenfilename(
+def pick_and_copy_file():
+    desktop_dir = os.path.join(os.path.expanduser("~"), "Desktop") # Establece el directorio inicial en el Escritorio del usuario
+    original_file_path = filedialog.askopenfilename(
         initialdir=desktop_dir,
         title="Selecciona un archivo",
         filetypes=(("Archivos permitidos", "*.reg *.log *.txt"),)
-    )
+    ) # Abre el cuadro de diálogo para seleccionar un archivo
+    
+    if not original_file_path:
+        print("No file selected")
+        return None
+    
+    filename, extension = os.path.splitext(os.path.basename(original_file_path)) # Obtiene el nombre y extension del archivo
+    input_folder_path = input_folder() # Crea/ obtiene el path al directorio de input
+    new_file_path = os.path.join(input_folder_path, f"{filename}{extension}") # Crear el path inicial
+    
+    i = 1
+    while os.path.exists(new_file_path): # Incrementar el sufijo si el archivo ya existe
+        new_file_path = os.path.join(input_folder_path, f"{filename} ({i}){extension}")
+        i += 1
+
+    shutil.copy(original_file_path, new_file_path) # Copiar el archivo al nuevo path
 
     # Imprime la ruta del archivo seleccionado (o None si no se seleccionó nada)
-    print("Archivo seleccionado:", file_path)
+    print("Archivo seleccionado:", original_file_path)
+    print("Archivo copiado:", new_file_path)
+    return new_file_path
+
+
+def input_folder():
+    # Obtener el directorio raíz del proyecto (el directorio actual)
+    project_root = os.getcwd()
+    
+    # Crear la carpeta "input" en el root del proyecto si no existe
+    input_folder = os.path.join(project_root, "input")
+    os.makedirs(input_folder, exist_ok=True)
+    return input_folder
