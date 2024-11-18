cdd184c6ab38b7225e1a7dd260560cc13afd9341
fiorde90 <fiorde_90@hotmail.com>
2024-11-18 20:04:50 -0300

    add output rules and file generation

--- src/single_scripts/new_script.py
+++ src/single_scripts/new_script.py
@@ -8,6 +8,8 @@ def start() :
     
     filter_inputs = get_filter_inputs() # Paso 1: Solicitar criterios de filtro
     
+    generate_outputs(file_path, filter_inputs) # Paso 2: Solicitar criterios de filtro
+
 
 def pick_and_copy_file():
     desktop_dir = os.path.join(os.path.expanduser("~"), "Desktop") # Establece el directorio inicial en el Escritorio del usuario
@@ -32,9 +34,6 @@ def pick_and_copy_file():
 
     shutil.copy(original_file_path, new_file_path) # Copiar el archivo al nuevo path
 
-    # Imprime la ruta del archivo seleccionado (o None si no se seleccionó nada)
-    print("Archivo seleccionado:", original_file_path)
-    print("Archivo copiado:", new_file_path)
     return new_file_path
 
 
@@ -50,8 +49,84 @@ def get_input_folder():
 def get_filter_inputs():
     print("Input filter criteria (Format example: \"NVDA ASUS MSFT, INTL\" will generate a file with msgs that contain first 3 items, and another one that contain INTL)")
     string_input = input() 
-    print(string_input)
     item_split_input = [item.strip().split() for item in string_input.split(",")]
-    print(item_split_input)
     return item_split_input
 
+def generate_outputs(file_path, filters):
+    header = []
+    unfiltered = []
+    filtered_files = {" ".join(key): [] for key in filters}
+    if len(filtered_files) == 1 and "" in filtered_files: filtered_files = dict()
+    securitylist = []
+    error_misc = []
+    
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        
+        i = 0
+        while i < len(lines):
+            line = lines[i].strip()
+            
+            if "35=y" in line:
+                securitylist.append(line)
+                i += 1
+                continue
+            
+            if "- USER CONNECTION" in line:
+                if i > 0:
+                    header.append(lines[i - 1].strip())
+                if len(error_misc) > 0:
+                    error_misc.pop()
+
+                header.append(line)
+                
+                for j in range(1, 9):
+                    if i + j < len(lines):
+                        header.append(lines[i + j].strip())
+                i += 9
+                continue
+
+            is_done = False
+            for key in filtered_files.keys():
+                for filter in key.split():
+                    if filter in line:
+                        filtered_files[key].append(line)
+                        is_done = True
+            if is_done: 
+                i += 1
+                continue
+                    
+            if "8=FIXT.1.1" in line:
+                unfiltered.append(line)
+                i += 1
+                continue
+
+            error_misc.append(line)
+            i += 1
+            
+    out_folder_path = get_out_folder(file_path)         
+    
+    generate_file(out_folder_path, "errors_misc", header, error_misc)
+    generate_file(out_folder_path, "securitylist", header, securitylist)
+    generate_file(out_folder_path, "unfiltered", header, unfiltered)
+    for key in filtered_files.keys():
+        generate_file(out_folder_path, f"filtered by {key}", header, filtered_files[key])
+    pass
+
+def get_out_folder(file_path):
+    # Obtener el directorio raíz del proyecto (el directorio actual)
+    project_root = os.getcwd()
+    
+    # Crear la carpeta "input" en el root del proyecto si no existe
+    folder = os.path.join(project_root, "out")
+    folder = os.path.join(folder, os.path.basename(file_path))
+    os.makedirs(folder, exist_ok=True)
+    return folder
+
+def generate_file(dir_path, name, header, content):
+    print(f"LENGTH CONTENT {name}: {len(content)}")
+    output_file_path = os.path.join(dir_path, f'{name}.log')
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        output_file.writelines("\n".join(header))
+        output_file.writelines("\n")
+        output_file.writelines("\n".join(content))
