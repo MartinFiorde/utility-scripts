271d6f305f33b24a5e62c1ddcdb98a81c3740baf
fiorde90 <fiorde_90@hotmail.com>
2024-11-18 18:23:41 -0300

    add filter selector

--- src/single_scripts/new_script.py
+++ src/single_scripts/new_script.py
@@ -3,10 +3,10 @@ import shutil
 from tkinter import Tk, filedialog, messagebox
 
 def start() :
-    
-    print("hello world")
-    file_path = pick_and_copy_file()
+    file_path = pick_and_copy_file() # Paso 0: Abrir el archivo a procesar
     if not file_path: return
+    
+    filter_inputs = get_filter_inputs() # Paso 1: Solicitar criterios de filtro
     
 
 def pick_and_copy_file():
@@ -22,7 +22,7 @@ def pick_and_copy_file():
         return None
     
     filename, extension = os.path.splitext(os.path.basename(original_file_path)) # Obtiene el nombre y extension del archivo
-    input_folder_path = input_folder() # Crea/ obtiene el path al directorio de input
+    input_folder_path = get_input_folder() # Crea/ obtiene el path al directorio de input
     new_file_path = os.path.join(input_folder_path, f"{filename}{extension}") # Crear el path inicial
     
     i = 1
@@ -38,11 +38,20 @@ def pick_and_copy_file():
     return new_file_path
 
 
-def input_folder():
+def get_input_folder():
     # Obtener el directorio raíz del proyecto (el directorio actual)
     project_root = os.getcwd()
     
     # Crear la carpeta "input" en el root del proyecto si no existe
-    input_folder = os.path.join(project_root, "input")
-    os.makedirs(input_folder, exist_ok=True)
-    return input_folder
+    folder = os.path.join(project_root, "input")
+    os.makedirs(folder, exist_ok=True)
+    return folder
+
+def get_filter_inputs():
+    print("Input filter criteria (Format example: \"NVDA ASUS MSFT, INTL\" will generate a file with msgs that contain first 3 items, and another one that contain INTL)")
+    string_input = input() 
+    print(string_input)
+    item_split_input = [item.strip().split() for item in string_input.split(",")]
+    print(item_split_input)
+    return item_split_input
+
