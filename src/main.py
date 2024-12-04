e12dd5a424a6af5a26ac48930796368799c9fba9
fiorde90 <fiorde_90@hotmail.com>
2024-12-04 11:35:37 -0300

    refactor: clear comments. no logic changed

--- src/main.py
+++ src/main.py
@@ -3,11 +3,6 @@ from src.single_scripts.fix_logs_filter import start
 
 def main():
     start()
-    
-    # REF OS SERVICES EXECUTION
-    # clean_public_dir()
-    # copy_static_into_public_dir()
-    # generate_pages_recursive()
 
 
 if __name__ == "__main__":
